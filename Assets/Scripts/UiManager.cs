79146f1bc496aeb0779511939a3fdac12f6e944d
Aashish Patel <patelaashish@sdlccorp.com>
2025-07-22 13:50:31 +0530

    Main Menu flow and Game Reload Added

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -3,22 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text turnsCountText;
     [SerializeField] private TMP_Text matchCountText;
+    [SerializeField] private Button homeButton;
     
     private void OnEnable()
     {
         LevelManager.TurnPlayed += UpdateTurnsCount;
         LevelManager.CardsMatched += UpdateMatchCount;
+        homeButton.onClick.AddListener(ReloadGame);
     }
 
     private void OnDisable()
     {
         LevelManager.TurnPlayed -= UpdateTurnsCount;
         LevelManager.CardsMatched -= UpdateMatchCount;
+        homeButton.onClick.RemoveListener(ReloadGame);
+    }
+
+    private void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void UpdateTurnsCount(int count)
